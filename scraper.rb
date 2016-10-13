60f118c2bea3e4359fa6d3cf599b2ddc72e82d30
Tony Bowden <tony@tmtm.com>
2016-10-13 13:17:24 +0100

    Add members from the 7th Convocation

--- scraper.rb
+++ scraper.rb
@@ -3,11 +3,19 @@
 
 require 'wikidata/fetcher'
 
-ru_names = EveryPolitician::Wikidata.wikipedia_xpath( 
+ru_6 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://ru.wikipedia.org/wiki/Государственная_дума_Федерального_собрания_Российской_Федерации_VI_созыва',
   xpath: '//h2/span[.="Список депутатов"]//following::table[1]//td[1]//a[not(@class="new")]/@title',
 )
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { ru: ru_names })
-warn EveryPolitician::Wikidata.notify_rebuilder
+# This will also pick up lots of things that aren't the people, but
+# that's OK; they'll get filtered out later anyway.
+ru_7 = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://ru.wikipedia.org/wiki/Список_депутатов_Государственной_думы_Российской_Федерации_VII_созыва',
+  after: '//h3/span[.="Депутаты, избранные по партийным спискам"]',
+  before: '//h2/span[.="Интересные факты"]',
+  xpath: '//a[not(@class="new")]/@title',
+)
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { ru: ru_6 | ru_7 })
 
